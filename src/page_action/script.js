88be9cf5a4e51c5a13bb4401883e6a77346f2280
Martin Becerra <carlos.m.becerra@intel.com>
2015-09-04 17:47:23 -0300

    Added ngjstree with octicons

--- src/page_action/script.js
+++ src/page_action/script.js
@@ -1,10 +1,66 @@
-angular.module('gitRecommender', [])
+angular.module('gitRecommender', ['ngJsTree'])
 	.service('Recommendations', Recommendations)
 	.controller('gitRecommenderCtrl', gitRecommenderCtrl);
 
 gitRecommenderCtrl.$inject = ['$scope', 'Recommendations'];
 
 function gitRecommenderCtrl($scope, Recommendations) {
+
+	$scope.treeConfig = {
+		core: {
+			multiple: false,
+			animation: true,
+			check_callback: true,
+			worker: true
+		},
+		types: {
+			default: {
+				icon: 'octicon octicon-flame'
+			},
+			folder: {
+				icon: 'octicon octicon-file-text'
+			},
+			file: {
+				icon: 'octicon octicon-file-directory'
+			}
+		},
+		plugins: ['types', 'wholerow']
+	};
+
+	$scope.treeData = [{
+		id: 'ajson1',
+		parent: '#',
+		text: 'Simple root node',
+		type: 'folder',
+		state: {
+			opened: true
+		}
+	}, {
+		id: 'ajson2',
+		parent: '#',
+		type: 'file',
+		text: 'Root node 2',
+		state: {
+			opened: true
+		}
+	}, {
+		id: 'ajson3',
+		type: 'default',
+		parent: 'ajson2',
+		text: 'Child 1',
+		state: {
+			opened: true
+		}
+	}, {
+		id: 'ajson4',
+		type: 'default',
+		parent: 'ajson2',
+		text: 'Child 2',
+		state: {
+			opened: true
+		}
+	}];
+
 	Recommendations.get().then(function(data) {
 		$scope.recommendations = data;
 	});
