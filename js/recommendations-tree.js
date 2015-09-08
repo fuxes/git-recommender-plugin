4d81e6017083f7f952141552e0e130d3ed611fd2
Martin Becerra <mrtnbcrr@gmail.com>
2015-09-08 11:26:27 -0300

    fixed function scope for Recommendations. Plugin working again

--- js/recommendations-tree.js
+++ js/recommendations-tree.js
@@ -12,64 +12,65 @@
 			get: getTree,
 			getConfig: jstreeConfig
 		};
-	}
 
-	//////////
+		//////////
 
-	function getTree() {
-		return Recommendations.get().then(function(data) {
-			var len = data.length,
-				nodes = [];
+		function getTree() {
+			return Recommendations.get().then(function(data) {
+				var len = data.length,
+					nodes = [];
 
-			for (var i = 0; i < len; i++) {
-				nodes.push(createNode(data[i]));
-			}
-
-			return nodes;
-		});
-	}
-
-	function jstreeConfig() {
-		var config = {
-			core: {
-				multiple: false,
-				animation: true,
-				check_callback: true,
-				worker: true
-			},
-			types: {
-				default: {
-					icon: 'octicon octicon-flame'
-				},
-				file: {
-					icon: 'octicon octicon-file-text'
-				},
-				dir: {
-					icon: 'octicon octicon-file-directory'
+				for (var i = 0; i < len; i++) {
+					nodes.push(createNode(data[i]));
 				}
-			},
-			// sort:
-			plugins: [
-				'sort',
-				'types',
-				'wholerow'
-			]
-		};
 
-		return config;
+				return nodes;
+			});
+		}
+
+		function jstreeConfig() {
+			var config = {
+				core: {
+					multiple: false,
+					animation: true,
+					check_callback: true,
+					worker: true
+				},
+				types: {
+					default: {
+						icon: 'octicon octicon-flame'
+					},
+					file: {
+						icon: 'octicon octicon-file-text'
+					},
+					dir: {
+						icon: 'octicon octicon-file-directory'
+					}
+				},
+				// sort:
+				plugins: [
+					'sort',
+					'types',
+					'wholerow'
+				]
+			};
+
+			return config;
+		}
+
+		function createNode(file) {
+			var node = {
+				id: file.sha,
+				text: file.name,
+				type: file.type,
+				parent: '#',
+				a_attr: {
+					href: file.html_url,
+				}
+			};
+
+			return node;
+		}
 	}
 
-	function createNode(file) {
-		var node = {
-			id: file.sha,
-			text: file.name,
-			type: file.type,
-			parent: '#',
-			a_attr: {
-				href: file.html_url,
-			}
-		};
-
-		return node;
-	}
 })();
